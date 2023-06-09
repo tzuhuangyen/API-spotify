d7a13d046ae2354fbd18040cb148dea5ba0b800f
tzuhuangyen <912yan@gmail.com>
2023-06-09 20:08:47 +0200

    token

--- token.js
+++ token.js
@@ -1,20 +1,35 @@
-const clientId = 'ZDE4NjAzZTk0NjRlNDBjMmJjZDEzOTUwYzE2ZDVkMTA=';
-const clientSecret = 'YWY4ZjllNjk1OGQ1NDdmOGJmNDhiYjZlNGZmY2JmMGI=';
+// import axios from 'axios';
+import express from './node_modules/express';
+const app = express();
 
-const authOptions = {
-  url:'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization':'Basic ' + (from(client_id + ':' + client_secret).toString('base64')),
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  form: {
-    grant_type: 'client_credentials',
-
-  },
-  json:true
-};
-  axios.post(authOptions, function(error,response,body){
-    if (!error && response.statusCode === 200) {
-      var token = body.access_token;
-    }
+app.post('/spotify/token', (req, res) => {
+  const clientId = 'd18603e9464e40c2bcd13950c16d5d10';
+  const clientSecret = 'af8f9e6958d547f8bf48bb6e4ffcbf0b';
+   const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+  };
+   axios.post(authOptions.url, authOptions.form, {
+    headers: authOptions.headers
+  }).then(response => {
+    const access_token = response.data.access_token;
+    res.send({ access_token });
+  }).catch(error => {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
   });
+});
+
+const port = 3000;
+ app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+
